80fcdba5aff6d1d70e73d73cb4acd8b9d6642921
sshunter <17326282+sshunter@users.noreply.github.com>
2022-02-09 00:46:14 +0000

    Deploying to main from @ sshunter/bcc-magic@a5984dba51877c29ea393a3a2944a6999427ab8e 🚀

--- games/bcc-magic/js/sw.js
+++ games/bcc-magic/js/sw.js
@@ -1,3 +1,3 @@
 /*! bcc-magic v0.3.0 | (c) 2022 Sam Hunter | All Rights Reserved */
-!function(){"use strict";self.addEventListener("install",(function(s){var i,n,e,t=null!==(e=null===(n=null===(i=s.target.serviceWorker)||void 0===i?void 0:i.scriptURL)||void 0===n?void 0:n.replace("/js/sw.js",""))&&void 0!==e?e:"";s.waitUntil(caches.open("bcc-magic").then((function(s){return s.addAll([t+"/index.html",t+"/css/main.css",t+"/js/main.js",t+"/js/sw.js"])})))}))}();
+!function(){"use strict";self.addEventListener("install",(function(s){var e,i,n,t=null!==(n=null===(i=null===(e=s.target.serviceWorker)||void 0===e?void 0:e.scriptURL)||void 0===i?void 0:i.replace("/js/sw.js",""))&&void 0!==n?n:"";s.waitUntil(caches.open("bcc-magic").then((function(s){return s.addAll([t+"/",t+"/index.html",t+"/css/main.css",t+"/js/main.js",t+"/js/sw.js",t+"/svg/refresh.svg",t+"/svg/restart.svg"])})))}))}();
 //# sourceMappingURL=sw.js.map
