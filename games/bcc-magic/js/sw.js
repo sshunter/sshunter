c8356989514aa98ac2a8f016ef9887c7d15ed5d2
sshunter <17326282+sshunter@users.noreply.github.com>
2022-02-09 02:06:14 +0000

    Deploying to main from @ sshunter/bcc-magic@0a0d12556cca97126a77a0af88b335027fc76c4a 🚀

--- games/bcc-magic/js/sw.js
+++ games/bcc-magic/js/sw.js
@@ -1,3 +1,3 @@
-/*! bcc-magic v0.3.0 | (c) 2022 Sam Hunter | All Rights Reserved */
+/*! bcc-magic v0.4.0 | (c) 2022 Sam Hunter | All Rights Reserved */
 !function(){"use strict";self.addEventListener("install",(function(s){var e,i,n,t=null!==(n=null===(i=null===(e=s.target.serviceWorker)||void 0===e?void 0:e.scriptURL)||void 0===i?void 0:i.replace("/js/sw.js",""))&&void 0!==n?n:"";s.waitUntil(caches.open("bcc-magic").then((function(s){return s.addAll([t+"/",t+"/index.html",t+"/css/main.css",t+"/js/main.js",t+"/js/sw.js",t+"/svg/refresh.svg",t+"/svg/restart.svg"])})))}))}();
 //# sourceMappingURL=sw.js.map
